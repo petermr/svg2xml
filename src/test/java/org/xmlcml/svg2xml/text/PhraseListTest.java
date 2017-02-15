3f8de4fdd0703b9031841171e81b16fc5fe96bfe
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-02-15 11:26:11 +0000

    fixed font width in tests

--- src/test/java/org/xmlcml/svg2xml/text/PhraseListTest.java
+++ src/test/java/org/xmlcml/svg2xml/text/PhraseListTest.java
@@ -1,6 +1,5 @@
 package org.xmlcml.svg2xml.text;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Level;
@@ -8,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.xmlcml.euclid.Real2;
+import org.xmlcml.euclid.Real2Range;
 import org.xmlcml.graphics.svg.SVGText;
 
 public class PhraseListTest {
@@ -18,14 +18,14 @@ public class PhraseListTest {
 	}
 	
 	@Test
-	// the bounding boxes are font dependent, so omit them
 	public void testCreatePhraseList() {
 		SVGText t11 = new SVGText(new Real2(25., 16.), "+");
 		t11.setFontSize(8.0);
+		t11.setSVGXFontWidth(234.);
 		Word w11 = new Word(t11);
 		Assert.assertEquals(""
 				+ "<g xmlns=\"http://www.w3.org/2000/svg\" class=\"word\">"
-				+   "<text stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\">+</text>"
+				+   "<text xmlns:svgx=\"http://www.xml-cml.org/schema/svgx\" stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\" svgx:width=\"234.0\">+</text>"
 				+ "</g>",  w11.toXML());
 		
 		Phrase p11 = new Phrase(w11);
@@ -33,17 +33,18 @@ public class PhraseListTest {
 		Assert.assertEquals(""
 				+ "<g xmlns=\"http://www.w3.org/2000/svg\" class=\"phrase\" string-value=\"+\">"
 				+ "<g class=\"word\">"
-				+ "<text stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\">+</text>"
+				+ "<text xmlns:svgx=\"http://www.xml-cml.org/schema/svgx\" stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\" svgx:width=\"234.0\">+</text>"
 				+ "</g>"
 				+ "</g>",  p11.toXML());
-//		Assert.assertEquals("((25.0,31.359375),(8.0,16.0))", p11.getOrCreateBoundingBox().toString());
+		Real2Range bbox = p11.getOrCreateBoundingBox();
+		Assert.assertEquals("((25.0,26.872),(8.0,16.0))", bbox.toString());
 		List<Word> wordList = p11.getOrCreateWordList();
 		Assert.assertEquals(1,  wordList.size());
 		Word word = wordList.get(0);
 		word.getStringValue();
 		Assert.assertEquals(""
 				+ "<g xmlns=\"http://www.w3.org/2000/svg\" class=\"word\" string-value=\"+\">"
-				+   "<text stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\">+</text>"
+				+   "<text xmlns:svgx=\"http://www.xml-cml.org/schema/svgx\" stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\" svgx:width=\"234.0\">+</text>"
 				+ "</g>",  word.toXML());
 		
 		PhraseList phraseList11 = new PhraseList();
@@ -54,11 +55,11 @@ public class PhraseListTest {
 		Assert.assertEquals(""
 				+ "<g xmlns=\"http://www.w3.org/2000/svg\" class=\"phrase\" string-value=\"+\">"
 				+ "<g class=\"word\">"
-				+ "<text stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\">+</text>"
+				+ "<text xmlns:svgx=\"http://www.xml-cml.org/schema/svgx\" stroke=\"none\" x=\"25.0\" y=\"16.0\" font-size=\"8.0\" svgx:width=\"234.0\">+</text>"
 				+ "</g>"
 				+ "</g>",
 				phrase.toXML());
-//		Assert.assertEquals("((25.0,31.359375),(8.0,16.0))", phraseList11.getOrCreateBoundingBox().toString());
+		Assert.assertEquals("((25.0,26.872),(8.0,16.0))", phraseList11.getOrCreateBoundingBox().toString());
 	}
 
 }
