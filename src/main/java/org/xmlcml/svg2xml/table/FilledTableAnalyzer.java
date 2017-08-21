a864ab7ca054356f2a23b43e7bdb4c402a173136
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2017-08-21 19:16:58 +0100

    adding cell analysis in tables

--- src/main/java/org/xmlcml/svg2xml/table/FilledTableAnalyzer.java
+++ src/main/java/org/xmlcml/svg2xml/table/FilledTableAnalyzer.java
@@ -1,25 +1,24 @@
 package org.xmlcml.svg2xml.table;
 
 import java.io.File;
-
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.xmlcml.euclid.Int2Range;
 import org.xmlcml.euclid.IntRange;
 import org.xmlcml.euclid.Real2;
 import org.xmlcml.euclid.Real2Range;
+import org.xmlcml.euclid.Real2RangeComparator;
 import org.xmlcml.euclid.RealRange.Direction;
 import org.xmlcml.graphics.svg.SVGElement;
 import org.xmlcml.graphics.svg.SVGG;
 import org.xmlcml.graphics.svg.SVGLine;
 import org.xmlcml.graphics.svg.SVGRect;
-import org.xmlcml.graphics.svg.SVGSVG;
 import org.xmlcml.graphics.svg.SVGText;
 
 import com.google.common.collect.ArrayListMultimap;
@@ -49,11 +48,14 @@ public class FilledTableAnalyzer {
 	private static final double YLINE_MIN= 10.0;
 
 	private SVGElement svgInElement;
-	private SVGSVG svgSvg;
 	private String colCssValue = DEFAULT_TEXT_CSS;
 	private String rowCssValue = DEFAULT_TEXT_CSS;
-	private String colBoundaryCss = DEFAULT_BOUNDARY_CSS;
-	private String rowBoundaryCss = DEFAULT_BOUNDARY_CSS;
+	private String colTickMarkCss = DEFAULT_BOUNDARY_CSS;
+	private String rowTickMarkCss = DEFAULT_BOUNDARY_CSS;
+	private List<IntRange> rowYRangeList;
+	private Multimap<IntRange, SVGRect> rectByIntYRange;
+	private List<Integer> rowYTickMarkList;
+	private List<Integer> colXTickMarkList;
 
 	public FilledTableAnalyzer() {
 		
@@ -63,96 +65,203 @@ public class FilledTableAnalyzer {
 		svgInElement = SVGElement.readAndCreateSVG(svgFile);
 	}
 	
-	public void createRows() {
-		Multimap<IntRange, SVGRect> rectByIntYRange = createRectByIntRange();
+	/** creates the colpspans/rowspans. now must turn these into rowGroups.
+	 * 
+	 */
+	public SVGG createBoundaryListsAndProcessRows() {
 		
-		svgSvg = new SVGSVG();
-		SVGG g = new SVGG();
+		SVGG gg = new SVGG();
 		
-		List<IntRange> rowRangeList = IntRange.createSortedList(rectByIntYRange.keySet());
-		List<Integer> rowBoundaryList = createRowBoundaryList(rowRangeList);
+		createBoundaryTickMarkLists();
+		SVGG g = drawHorizontalTicksForRows();
+		gg.appendChild(g);
+		g = drawVerticalTicksForColumns();
+		gg.appendChild(g);
+		createAndOutputColspanCountForSpanningCells();
 		
-		drawRowBoundaries(g, rowBoundaryList);
-
-		List<Integer> colBoundaryList = createColumnBoundaryList(rectByIntYRange, rowRangeList);
-		
-		createAndDrawColspans(rectByIntYRange, g, rowRangeList, rowBoundaryList, colBoundaryList);
-		drawColumnBoundaries(g, colBoundaryList);
-		
+		g = new SVGG();
 		List<SVGText> textList = SVGText.extractSelfAndDescendantTexts(svgInElement);
 		for (SVGText text : textList) {
 			g.appendChild(text.copy());
 		}
-		svgSvg.appendChild(g);
+		gg.appendChild(g);
+		return gg;
 	}
 
-	private Multimap<IntRange, SVGRect> createRectByIntRange() {
-		List<SVGRect> rectList = SVGRect.extractSelfAndDescendantRects(svgInElement);
-		Multimap<IntRange, SVGRect> rectByIntYRange = ArrayListMultimap.create();
-		for (SVGRect rect : rectList) {
-			rect.setBoundingBoxCached(true);
-			Real2Range r2range = rect.getBoundingBox();
-			Int2Range i2range = new Int2Range(r2range);
-			IntRange irange = new IntRange(r2range.getRealRange(Direction.VERTICAL));
-			rectByIntYRange.put(irange, rect);
+	public List<CellRow> createCellRowList() {
+		List<CellRow> cellRowList = new ArrayList<CellRow>();
+ 		createBoundaryTickMarkLists();
+		for (IntRange rowYRange : rowYRangeList) {
+			List<SVGRect> rectsInYRow = new ArrayList<SVGRect>(rectByIntYRange.get(rowYRange));
+			CellRow cellRow = new CellRow(rectsInYRow, colXTickMarkList);
+			LOG.debug(cellRow.getColSpanCounts());
+			cellRowList.add(cellRow);
 		}
-		return rectByIntYRange;
+		return cellRowList;
+	}
+
+	private void createBoundaryTickMarkLists() {
+		List<SVGRect> rectList = SVGRect.extractSelfAndDescendantRects(svgInElement);
+		rectByIntYRange = createRectByYIntRange(rectList);
+		rowYRangeList = IntRange.createSortedList(rectByIntYRange.keySet());
+		rowYTickMarkList = createTickMarkListFromRanges(rowYRangeList);
+		colXTickMarkList = createColumnXTickMarkList(rectByIntYRange, rowYRangeList);
 	}
 	
-	private void createAndDrawColspans(Multimap<IntRange, SVGRect> rectByIntYRange, SVGG g, List<IntRange> rowRangeList,
-			List<Integer> rowBoundaryList, List<Integer> colBoundaryList) {
-		for (IntRange rowRange : rowRangeList) {
-			List<SVGRect> rowList = new ArrayList<SVGRect>(rectByIntYRange.get(rowRange));
-			for (SVGRect rowRect : rowList) {
-				SVGRect r1 = new SVGRect(rowRect);
-				g.appendChild(r1);
-				Real2Range bbox = r1.getBoundingBox();
-				createAndOutputColspans(g, colBoundaryList, bbox);
-				createAndOutputRowspans(g, rowBoundaryList, bbox);
+	// =======================
+
+
+	// drawing routines ==================
+
+	/** adds Cell to graphic output and annotates with row/col span count
+	 * 
+	 * @return
+	 */
+	private SVGG createAndOutputColspanCountForSpanningCells() {
+		SVGG g = new SVGG();
+		// iterate over rows
+		for (IntRange rowRange : rowYRangeList) {
+			List<SVGRect> rectsInRowList = new ArrayList<SVGRect>(rectByIntYRange.get(rowRange));
+			for (SVGRect rectInRow : rectsInRowList) {
+				SVGRect rectCopy = new SVGRect(rectInRow);
+				g.appendChild(rectCopy);
+				Real2Range cellBbox = rectCopy.getBoundingBox();
+				SVGG gg = createAndOutputColspanCountForSpannningCell(cellBbox);
+				g.appendChild(gg);
+				gg = createAndOutputRowspanCountForSpanningRow(cellBbox);
+				g.appendChild(gg);
 			}
 		}
+		return g;
 	}
 
-	private void createAndOutputRowspans(SVGG g, List<Integer> rowBoundaryList, Real2Range bbox) {
-		IntRange yrange = new IntRange(bbox.getYRange());
-		int rowspans = createSpanCounts(yrange, rowBoundaryList);
+
+	/** adds SVGText with rowspan count to any boxes with rowspan > 1
+	 * 
+	 * @param cellBbox
+	 * @return
+	 */
+	private SVGG createAndOutputRowspanCountForSpanningRow(Real2Range cellBbox) {
+		SVGG g = new SVGG();
+		IntRange yrange = new IntRange(cellBbox.getYRange());
+		int rowspans = createSpanCounts(yrange, rowYTickMarkList);
 		if (rowspans > 1) {
-			SVGText rowspanText = new SVGText(bbox.getAllCornerPoints()[3], ROWSPAN+rowspans);
+			SVGText rowspanText = new SVGText(cellBbox.getAllCornerPoints()[3], ROWSPAN+rowspans);
 			rowspanText.setCSSStyle(rowCssValue);
 			g.appendChild(rowspanText);
 		}
+		return g;
 	}
 
-	private void createAndOutputColspans(SVGG g, List<Integer> colBoundaryList, Real2Range bbox) {
-		IntRange xrange = new IntRange(bbox.getXRange());
-		int colspans = createSpanCounts(xrange, colBoundaryList);
+	/** adds SVGText with colspan count to any boxes with colspan > 1
+	 * 
+	 * @param cellBbox
+	 * @return
+	 */
+
+	private SVGG createAndOutputColspanCountForSpannningCell(Real2Range cellBbox) {
+		SVGG g = new SVGG();
+		IntRange xrange = new IntRange(cellBbox.getXRange());
+		int colspans = createSpanCounts(xrange, colXTickMarkList);
 		if (colspans > 1) {
-			SVGText colspanText = new SVGText(bbox.getAllCornerPoints()[3], COLSPAN+colspans);
+			SVGText colspanText = new SVGText(cellBbox.getAllCornerPoints()[3], COLSPAN+colspans);
 			colspanText.setCSSStyle(colCssValue);
 			g.appendChild(colspanText);
 		}
+		return g;
 	}
 
-	private static List<Integer> createRowBoundaryList(List<IntRange> rowRangeList) {
-		Set<Integer> rowBoundarySet = new HashSet<Integer>();
-		for (IntRange yrange : rowRangeList) {
-			int min = yrange.getMin();
-			int max = yrange.getMax();
-			rowBoundarySet.add(min);
-			rowBoundarySet.add(max);
+
+	private SVGG drawVerticalTicksForColumns() {
+		SVGG g = new SVGG();
+		for (Integer jcol : colXTickMarkList) {
+			SVGLine line = new SVGLine(new Real2(jcol, YLINE_MIN), new Real2(jcol, YLINE_MAX));
+			line.setCSSStyle(colTickMarkCss);
+			g.appendChild(line);
 		}
-		List<Integer> rowBoundaryList = new ArrayList<Integer>(rowBoundarySet);
-		Collections.sort(rowBoundaryList);
-		return rowBoundaryList;
+		return g;
 	}
 
-	private int createSpanCounts(IntRange range, List<Integer> boundaryList) {
+	private SVGG drawHorizontalTicksForRows() {
+		SVGG g = new SVGG();
+		for (Integer irow : rowYTickMarkList) {
+			SVGLine line = new SVGLine(new Real2(XLINE_MIN, irow), new Real2(XLINE_MAX, irow));
+			line.setCSSStyle(rowTickMarkCss);
+			g.appendChild(line);
+		}
+		return g;
+	}
+
+	// private static =========================
+	
+	private static Multimap<IntRange, SVGRect> createRectByYIntRange(List<SVGRect> rectList) {
+		Multimap<IntRange, SVGRect> rectByYIntRange = ArrayListMultimap.create();
+		for (SVGRect rect : rectList) {
+			rect.setBoundingBoxCached(true);
+			Real2Range r2range = rect.getBoundingBox();
+			IntRange irange = new IntRange(r2range.getRealRange(Direction.VERTICAL));
+			rectByYIntRange.put(irange, rect);
+		}
+		return rectByYIntRange;
+	}
+	
+
+	/**
+	 * Iterate over all rowYRanges , retrieve Rects and create XTickMarks
+	 * This could be used elsewhere.
+	 * 
+	 * @param rectByIntYRange rects indexed by YRange (rows)
+	 * @param rowYRangeList range or rows to index over
+	 * @return
+	 */
+	private static List<Integer> createColumnXTickMarkList(Multimap<IntRange, SVGRect> rectByIntYRange, List<IntRange> rowYRangeList) {
+		Set<IntRange> colXRangeSet = new HashSet<IntRange>();
+		for (IntRange rowYRange : rowYRangeList) {
+			List<SVGRect> rectsInRowList = new ArrayList<SVGRect>(rectByIntYRange.get(rowYRange));
+			for (SVGRect rectInRow : rectsInRowList) {
+				IntRange colXRange = new IntRange(rectInRow.getBoundingBox().getXRange());
+				colXRangeSet.add(colXRange);
+			}
+		}
+		List<Integer> colXTickMarkList = createTickMarkListFromRanges(IntRange.createSortedList(colXRangeSet));
+		return colXTickMarkList;
+	}
+
+
+	
+	/** create list of sorted coordinates delineating rows or columns.
+	 * iterates over IntRange list (either X or Y ) and creates a set
+	 * then sort it into tickMarks
+	 * 
+	 * @param rangeList
+	 * @return
+	 */
+	private static List<Integer> createTickMarkListFromRanges(List<IntRange> rangeList) {
+		Set<Integer> tickMarkSet = new HashSet<Integer>();
+		for (IntRange range : rangeList) {
+			int min = range.getMin();
+			int max = range.getMax();
+			tickMarkSet.add(min);
+			tickMarkSet.add(max);
+		}
+		List<Integer> tickMarkList = new ArrayList<Integer>(tickMarkSet);
+		Collections.sort(tickMarkList);
+		return tickMarkList;
+	}
+
+	/** 
+	 * 
+	 * @param range
+	 * @param tickMarkList 
+	 * @return number of cells in tickMarkList spanned by range
+	 */
+	private static int createSpanCounts(IntRange range, List<Integer> tickMarkList) {
 		Integer min = range.getMin();
 		Integer max = range.getMax();
-		int idx = boundaryList.indexOf(min);
+		int idx = tickMarkList.indexOf(min);
 		if (idx >= 0) {
-			for (int i = idx + 1; i < boundaryList.size(); i++) {
-				if (boundaryList.get(i).equals(max)) {
+			for (int i = idx + 1; i < tickMarkList.size(); i++) {
+				if (tickMarkList.get(i).equals(max)) {
 					return i - idx;
 				}
 			}
@@ -160,44 +269,7 @@ public class FilledTableAnalyzer {
 		return 0;
 	}
 
-	private List<Integer> createColumnBoundaryList(Multimap<IntRange, SVGRect> rectByIntYRange, List<IntRange> rowRangeList) {
-		Set<IntRange> colRangeSet = new HashSet<IntRange>();
-		for (IntRange rowRange : rowRangeList) {
-			List<SVGRect> rowList = new ArrayList<SVGRect>(rectByIntYRange.get(rowRange));
-			for (SVGRect rowRect : rowList) {
-				IntRange colRange = new IntRange(rowRect.getBoundingBox().getXRange());
-				colRangeSet.add(colRange);
-			}
-		}
-		List<Integer> colBoundaryList = createSortedColumnBoundaryList(colRangeSet);
-		return colBoundaryList;
-	}
 
-	private void drawColumnBoundaries(SVGG g, List<Integer> colBoundaryList) {
-		for (int i = 0; i < colBoundaryList.size(); i++) {
-			SVGLine line = new SVGLine(new Real2(colBoundaryList.get(i), YLINE_MIN), new Real2(colBoundaryList.get(i), YLINE_MAX));
-			line.setCSSStyle(colBoundaryCss);
-			g.appendChild(line);
-		}
-	}
-
-	private void drawRowBoundaries(SVGG g, List<Integer> rowBoundaryList) {
-		for (Integer rowBoundary : rowBoundaryList) {
-			SVGLine line = new SVGLine(new Real2(XLINE_MIN, rowBoundary), new Real2(XLINE_MAX, rowBoundary));
-			line.setCSSStyle(rowBoundaryCss);
-			g.appendChild(line);
-		}
-	}
-
-	private static List<Integer> createSortedColumnBoundaryList(Set<IntRange> colRangeSet) {
-		List<IntRange> colRangeList = IntRange.createSortedList(colRangeSet);
-		List<Integer> colBoundaryList = createRowBoundaryList(colRangeList);
-		return colBoundaryList;
-	}
-
-	public SVGSVG createSVGSVG() {
-		return svgSvg;
-	}
 
 
 }
