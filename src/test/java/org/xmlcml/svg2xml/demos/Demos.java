a1e5d9111e9c5914bd36e87325387882008ca87e
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2018-01-24 13:34:32 +0000

    added Demos in test

--- src/test/java/org/xmlcml/svg2xml/demos/Demos.java
+++ src/test/java/org/xmlcml/svg2xml/demos/Demos.java
@@ -1,6 +1,6 @@
 package org.xmlcml.svg2xml.demos;
 
-import org.xmlcml.svg2xml.pdf.PDFAnalyzerTestOLD;
+import org.xmlcml.svg2xml.pdf.PDFAnalyzerTest;
 
 public class Demos {
 
@@ -8,21 +8,21 @@ public class Demos {
 //			ebola1();
 //			astro1();
 //			plot1();
-			PDFAnalyzerTestOLD.analyzePDF("demos/gandhi/sample.pdf"); 
+			PDFAnalyzerTest.analyzePDF("demos/gandhi/sample.pdf"); 
 
 	}
 
 	private static void ebola1() {
-		PDFAnalyzerTestOLD.analyzePDF(("demos/ebola/roadmapsitrep_12Nov2014_eng.pdf")); 
+		PDFAnalyzerTest.analyzePDF(("demos/ebola/roadmapsitrep_12Nov2014_eng.pdf")); 
 //		PDFAnalyzerTest.analyzePDF(("demos/ebola/roadmapsitrep_14Nov2014_eng.pdf")); 
 	}
 	
 	private static void astro1() {
-		PDFAnalyzerTestOLD.analyzePDF(("demos/astro/0004-637X_778_1_1.pdf")); 
+		PDFAnalyzerTest.analyzePDF(("demos/astro/0004-637X_778_1_1.pdf")); 
 	}
 	
 	private static void plot1() {
-		PDFAnalyzerTestOLD.analyzePDF(("demos/plot/22649_Sada_2012-1.pdf")); 
+		PDFAnalyzerTest.analyzePDF(("demos/plot/22649_Sada_2012-1.pdf")); 
 //		analyzePDFFile
 	}
 	
